87b85ede8af6be9217d388f3d8e2db8712402c42
Luca Perret <perret.luca@gmail.com>
2018-02-19 23:04:36 +0100

    fix(entrypoint): verifying existing app folder

--- strapi.sh
+++ strapi.sh
@@ -3,12 +3,16 @@ set -ea
 
 cd /usr/src/api
 
+APP_NAME=${APP_NAME:-strapi-app}
 DATABASE_CLIENT=${DATABASE_CLIENT:-mongo}
 DATABASE_HOST=${DATABASE_HOST:-localhost}
 DATABASE_PORT=${DATABASE_PORT:-27017}
 DATABASE_NAME=${DATABASE_NAME:-strapi}
 
-strapi new strapi-api --dbclient=$DATABASE_CLIENT --dbhost=$DATABASE_HOST --dbport=$DATABASE_PORT --dbname=$DATABASE_NAME --dbusername=$DATABASE_USERNAME --dbpassword=$DATABASE_PASSWORD
+if [ ! -f "$APP_NAME/package.json" ]
+then
+    strapi new ${APP_NAME} --dbclient=$DATABASE_CLIENT --dbhost=$DATABASE_HOST --dbport=$DATABASE_PORT --dbname=$DATABASE_NAME --dbusername=$DATABASE_USERNAME --dbpassword=$DATABASE_PASSWORD
+fi
 
-cd strapi-api
+cd $APP_NAME
 npm start
