dd62f30d01ad9a39766d9ab708165dd2bdb51168
Alexandre Bodin <bodin.alex@gmail.com>
2019-10-17 19:02:49 +0200

    Rework docker images for beta version

---
+++ bin/build.js
@@ -0,0 +1,166 @@
+#!/usr/bin/env node
+const path = require('path');
+
+const yargs = require('yargs');
+const execa = require('execa');
+const semver = require('semver');
+
+const types = ['base', 'strapi'];
+const NODE_VERSIONS = [10, 12];
+const LATEST_NODE_VERSION = 12;
+
+function execDocker(args) {
+  console.log(`docker ${args.join(' ')}`);
+  return execa('docker', args, {
+    stdio: 'inherit',
+  });
+}
+
+async function buildBaseImages() {
+  for (nodeVersion of NODE_VERSIONS) {
+    await buildBaseImage({ nodeVersion });
+    await buildBaseImage({ nodeVersion, alpine: true });
+  }
+}
+
+async function buildBaseImage({ nodeVersion, alpine }) {
+  let tmpImg = `strapi/base:tmp`;
+
+  await execDocker([
+    'build',
+    '--build-arg',
+    `NODE_VERSION=${nodeVersion}${alpine ? '-alpine' : ''}`,
+    '-t',
+    tmpImg,
+    `./base${alpine ? '/alpine' : ''}`,
+  ]);
+
+  const tags = buildBaseTags({ nodeVersion, alpine });
+
+  for (let tag of tags) {
+    await execDocker(['tag', tmpImg, `strapi/base:${tag}`]);
+  }
+
+  await execDocker(['image', 'rm', tmpImg]);
+}
+
+async function buildStrapiImages(version) {
+  if (semver.valid(version) === null) {
+    throw new Error('Invalid strapi version provided: ' + version);
+  }
+
+  for (nodeVersion of NODE_VERSIONS) {
+    await buildStrapiImage({ nodeVersion, version });
+    await buildStrapiImage({ nodeVersion, version, alpine: true });
+  }
+}
+
+async function buildStrapiImage({ nodeVersion, version, alpine = false }) {
+  let tmpImg = `strapi/strapi:tmp`;
+
+  await execDocker([
+    'build',
+    '--build-arg',
+    `BASE_VERSION=${nodeVersion}${alpine ? '-alpine' : ''}`,
+    '--build-arg',
+    `STRAPI_VERSION=${version}`,
+    '-t',
+    tmpImg,
+    './strapi',
+  ]);
+
+  const tags = buildStrapiTags({ version, nodeVersion, alpine });
+
+  for (let tag of tags) {
+    await execDocker(['tag', tmpImg, `strapi/strapi:${tag}`]);
+  }
+
+  await execDocker(['image', 'rm', tmpImg]);
+}
+
+function buildStrapiTags({ version, nodeVersion, alpine = false }) {
+  let tags = [];
+  let versions = [version];
+
+  const major = semver.major(version);
+  const minor = semver.minor(version);
+  const patch = semver.patch(version);
+  const pre = semver.prerelease(version);
+
+  if (!pre) {
+    versions = [major, `${major}.${minor}`, `${major}.${minor}.${patch}`];
+  }
+
+  for (version of versions) {
+    tags.push(`${version}-node${nodeVersion}${alpine ? '-alpine' : ''}`);
+
+    if (nodeVersion === LATEST_NODE_VERSION) {
+      tags.push(`${version}${alpine ? '-alpine' : ''}`);
+    }
+  }
+
+  if (nodeVersion === LATEST_NODE_VERSION && !alpine) {
+    tags.push('latest');
+  }
+
+  if (nodeVersion === LATEST_NODE_VERSION && alpine) {
+    tags.push('alpine');
+  }
+
+  return tags;
+}
+
+function buildBaseTags({ nodeVersion, alpine = false }) {
+  let tags = [];
+
+  tags.push(`${nodeVersion}${alpine ? '-alpine' : ''}`);
+
+  if (nodeVersion === LATEST_NODE_VERSION && !alpine) {
+    tags.push('latest');
+  }
+
+  return tags;
+}
+
+const argv = yargs
+  .option('type', {
+    alias: 't',
+    describe: 'Which images to build (all,strapi,base)',
+    default: 'all',
+    type: 'string',
+  })
+  .option('strapi-version', {
+    describe: 'Which strapi version to build',
+    type: 'string',
+  })
+  .version(false)
+  .help('h')
+  .alias('h', 'help').argv;
+
+if (argv.help) {
+  yargs.showHelp();
+}
+
+async function run() {
+  switch (argv.type) {
+    case 'base': {
+      await buildBaseImages();
+      break;
+    }
+    case 'strapi': {
+      await buildStrapiImages(argv.strapiVersion);
+      break;
+    }
+    case 'all':
+    default: {
+      await buildBaseImages();
+      await buildStrapiImages(argv.strapiVersion);
+      break;
+    }
+  }
+}
+
+run().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
---
+++ strapi/docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+set -ea
+
+if [ "$1" = "strapi" ]; then
+
+  if [ ! -f "package.json" ]; then
+
+    DATABASE_CLIENT=${DATABASE_CLIENT:-sqlite}
+
+    EXTRA_ARGS=${EXTRA_ARGS}
+
+    echo "$ARGS"
+
+    strapi new . \
+      --dbclient=$DATABASE_CLIENT \
+      --dbhost=$DATABASE_HOST \
+      --dbport=$DATABASE_PORT \
+      --dbname=$DATABASE_NAME \
+      --dbusername=$DATABASE_USERNAME \
+      --dbpassword=$DATABASE_PASSWORD \
+      --dbssl=$DATABASE_SSL \
+      --dbssl=$DATABASE_SSL \
+      --dbsrv=$DATABASE_SRV \
+      --dbauth=$DATABASE_AUTH \
+      $EXTRA_ARGS
+
+  elif [ ! -d "node_modules" ]; then
+
+    if [ -f "yarn.lock" ]; then
+
+      yarn install
+
+    elif [ -f "package-json.lock" ]; then
+
+      npm install
+
+    fi
+
+  fi
+
+fi
+
+exec "$@"
